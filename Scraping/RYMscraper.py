20803a8619089bf25bd1c5f40688ca32c8a78241
Floor4821 <r1050388@student.thomasmore.be>
2025-03-26 14:41:43 +0100

    Initial Commit

---
+++ Scraping/RYMscraper.py
@@ -0,0 +1,8 @@
+import requests
+import cloudscraper
+from bs4 import BeautifulSoup
+url = "https://rateyourmusic.com/~Flora4281"
+scraper = cloudscraper.create_scraper()
+response = scraper.get(url)
+soup_object = BeautifulSoup(response.text)
+print(soup_object.get_text())
