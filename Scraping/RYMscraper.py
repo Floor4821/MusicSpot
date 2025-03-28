dd78776adb287a41e1340382efb6c3f03bfabcad
Jamey848 <r1042086@student.thomasmore.be>
2025-03-28 16:22:40 +0100

    MusicSpot wireframe 1

--- Scraping/RYMscraper.py
+++
@@ -1,9 +0,0 @@
-import requests
-import cloudscraper
-from bs4 import BeautifulSoup
-url = "https://rateyourmusic.com/~Flora4281"
-scraper = cloudscraper.create_scraper()
-response = scraper.get(url)
-soup_object = BeautifulSoup(response.text)
-print(soup_object.get_text())
-print("PLEASE JUST COMMIT")
