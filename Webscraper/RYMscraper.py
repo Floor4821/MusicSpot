c549fde418e13f24c9e38a3fe06d8d297875090e
Floor4821 <r1050388@student.thomasmore.be>
2025-05-05 14:04:52 +0200

    webscraper finished, data collected

--- Webscraper/RYMscraper.py
+++ Webscraper/RYMscraper.py
@@ -1,14 +1,48 @@
 import requests
+import datetime
 from bs4 import BeautifulSoup as BS
 import undetected_chromedriver as uc
 import time
+import base64
 import json
+import math
+import random
 
 options = uc.ChromeOptions()
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-blink-features=AutomationControlled")
-driver = uc.Chrome(version_main=134, options=options)
+driver = uc.Chrome(options=options)
 
+def get_songs(soup):
+	length = 0
+	processed_songs = []
+	songs = soup.find("ul", {"class": "tracks tracklisting"}).find_all("li", {"class": "track"})
+	for song in songs:
+		try: 
+			length = int(song.find("span", {"class": "tracklist_total"}).text.split(":")[1].strip())
+		except: 
+			try: 
+				song_title = song.find("a", {"class": "song"}).text
+				processed_songs.append(song_title)
+			except Exception as e: 
+				print(e)
+				continue
+	return processed_songs, length
+
+def get_products(length=40):
+	products = {
+		"Vinyl": None,
+		"CD": None,
+		"Cassette": None
+	}
+
+	for product in (("Vinyl", 50, 30.00), ("CD", 70, 15.00), ("Cassette", 90, 8.00)):
+		products[product[0]] = {
+			"Stock": random.randint(10, 200),
+			"Price": product[2] * math.ceil(length / product[1])
+		}
+
+	return products
 
 def get_releases(artist):
 	link_dict = {}
@@ -32,16 +66,154 @@ def get_releases(artist):
 			link_dict[t[1]] = link_list
 
 	return link_dict
+
+def get_releaseinfo(release_link, artist, format):
+	url = release_link
+	driver.get(url)
+	time.sleep(30)
+	release_page = driver.page_source
+	soup = BS(release_page, "lxml")
+
+	name = soup.find("div", {"class": "album_title"}).contents[0].strip()
+	genres = soup.find("span", {"class": "release_pri_genres"}).text.split(", ")
+	try: date_str = soup.find("span", {"class": "issue_year ymd"})["title"]
+	except: date_str = None
+	songs = get_songs(soup)[0]
+	length = get_songs(soup)[1]
+	if length != 0: products = get_products(length)
+	else: products = get_products()
+	try: img_link = "https:" + soup.find('img', attrs={'srcset': True})["srcset"].split(',')[0]
+	except: img_link = None
+
+
+	return {
+		"Link": url,
+		"Name": name,
+		"Artist": artist,
+		"Songs": songs,
+		"Genres": genres,
+		"Release Date": date_str,
+		"Release Type": format,
+		"Products": products,
+		"Cover": img_link
+	}
+
+def check_empty(list):
+	return all(str(s).strip() == "" for s in list)
     
 def main():
-	with open('Artists.json', 'r', encoding='utf-8') as file:
-		artist_dict = json.load(file)
+	genres = [
+		"Ambient",
+		"DarkAmbient",
+		"ElectronicAmbient",
+		"BitMusic",
+		"Breakbeat",
+		"BubblegumBass",
+		"Chillout",
+		"DrumAndBass",
+		"Dubstep",
+		"ElectroIndustrial",
+		"Electronic",
+		"GlitchHop",
+		"Hardcore",
+		"House",
+		"IDM",
+		"Indietronica",
+		"Techno",
+		"TripHop",
+		"Drone",
+		"Experimental",
+		"Noise",
+		"Plunderphonics",
+		"AvantFolk",
+		"ChamberFolk",
+		"FolkRock",
+		"IndieFolk",
+		"AbstractHipHop",
+		"ConsciousHipHop",
+		"ExperimentalHipHop",
+		"HardcoreHipHop",
+		"JazzRap",
+		"PopRap",
+		"Trap",
+		"AvantGardeJazz",
+		"CoolJazz",
+		"HardBop",
+		"JazzFusion",
+		"ModalJazz",
+		"SmoothJazz",
+		"AlternativeMetal",
+		"AvantGardeMetal",
+		"BlackMetal",
+		"DeathMetal",
+		"HeavyMetal",
+		"ProgressiveMetal",
+		"StonerMetal",
+		"ThrashMetal",
+		"ArtPop",
+		"BaroquePop",
+		"DancePop",
+		"Electropop",
+		"GlitchPop",
+		"IndiePop",
+		"JanglePop",
+		"ProgressivePop",
+		"PsychedelicPop",
+		"Synthpop",
+		"ArtPunk",
+		"Emo",
+		"GothicRock",
+		"HardcorePunk",
+		"PopPunk",
+		"PostHardcore",
+		"PostPunk",
+		"PunkRock",
+		"AlternativeRock",
+		"ArtRock",
+		"DreamPop",
+		"ExperimentalRock",
+		"GarageRock",
+		"Grunge",
+		"HardRock",
+		"IndieRock",
+		"IndustrialRock",
+		"MathRock",
+		"NewWave",
+		"NoiseRock",
+		"PopRock",
+		"PostRock",
+		"ProgressiveRock",
+		"PsychedelicRock",
+		"Shoegaze",
+		"SlackerRock",
+		"Slowcore",
+		"NeoSoul",
+		"ProgressiveSoul",
+		"Soul"
+	]
+	with open("edited_releases.json", "r", encoding="utf-8") as file:
+		releases = json.load(file)
+	
+	safe = []
+	unsafe = []
 
-	for artist in artist_dict:
-		if artist_dict[artist] == None:
-			artist_dict[artist] = get_releases(artist)
-		with open('Artists.json', 'w', encoding='utf-8') as file:
-			json.dump(artist_dict, file, indent=4)
+	for r in releases:
+		if r["Genres"] in genres:
+			safe.append(r)
+		else: unsafe.append(r)
+
+	print(len(safe))
+	print(len(unsafe))
+
+	with open("safe.json", "w", encoding="utf-8") as file:
+		json.dump(safe, file, indent=4, ensure_ascii=False)
+
+	with open("unsafe.json", "w", encoding="utf-8") as file:
+		json.dump(unsafe, file, indent=4, ensure_ascii=False)	
+		
+
+
+
 
 if __name__ == "__main__":
     main()
